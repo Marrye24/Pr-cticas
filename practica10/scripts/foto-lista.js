44b605bece8bb7d350dc21db854072652bfbae21
María José M <95242045+Marrye24@users.noreply.github.com>
2023-03-29 20:55:18 -0600

    Add files via upload

---
+++ practica10/scripts/foto-lista.js
@@ -0,0 +1,11 @@
+const PHOTO_LIST = [
+    'img/conejo.jpg', 
+    'img/lobo.jpg', 
+    'img/gato.jpg',
+    'img/mono.jpg',
+    'img/panda.jpg',
+    'img/perro.jpeg',
+    'img/suricata.jpg', 
+    'img/tigre.jpg',
+    'img/zorro.jpg'
+];
